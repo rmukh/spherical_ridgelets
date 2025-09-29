a379366d10ed2367f25727a3faa0f7a8923bdfb0
Rinat M <25082858+rmukh@users.noreply.github.com>
2025-09-29 08:58:24 +0800

    Update linux_standalone_build.sh

--- linux_standalone_build.sh
+++ linux_standalone_build.sh
@@ -10,13 +10,13 @@ then
 	n_threads=4
 fi
 
-if [[ $n_threads =~ ^-?[0-9]+$ ]] && [ $n_threads -gt 0 ];
+if [[ "$n_threads" =~ ^-?[0-9]+$ ]] && [ "$n_threads" -gt 0 ];
 then
-	echo Start building using $n_threads threads
+	echo Start building using "$n_threads" threads
 	mkdir build
 	cd build
 	cmake .. -DJUST_BUILD=1 -D CMAKE_C_COMPILER=gcc-7 -D CMAKE_CXX_COMPILER=g++-7
-	make -j$n_threads CC=gcc-7 CPP=g++-7 CXX=g++-7 LD=g++-7
+	make -j"{$n_threads}" CC=gcc-7 CPP=g++-7 CXX=g++-7 LD=g++-7
 else
 	echo Incorrent number of threads
 fi
