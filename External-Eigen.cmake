646c687438766da17e9038714e413f71c69f6b22
Rinat M <25082858+rmukh@users.noreply.github.com>
2025-09-29 13:22:46 +0800

    Update External-Eigen.cmake

--- External-Eigen.cmake
+++ External-Eigen.cmake
@@ -2,7 +2,7 @@
 # Get and build Eigen
 
 set(EIGEN_TAG "for/itk-20231108-master-4d54c43d")
-
+set(EIGEN3_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/eigen/install)
 option(eigen_PATCH_FILE "Patch file to apply to eigen" OFF)
 set(eigen_PATCH_COMMAND "")
 if(eigen_PATCH_FILE)
@@ -13,17 +13,20 @@ ExternalProject_Add(Eigen3
     PREFIX Eigen3
     GIT_REPOSITORY "https://github.com/InsightSoftwareConsortium/eigen"
     GIT_TAG "${EIGEN_TAG}"
-    INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/eigen/install
+    INSTALL_DIR ${EIGEN3_INSTALL_DIR}
     ${eigen_PATCH_COMMAND}
     LOG_DOWNLOAD ON
     LOG_OUTPUT_ON_FAILURE ON
     CMAKE_ARGS
-        -DCMAKE_BUILD_TYPE:STRING=Release
+        -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
         -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF
-        -DCMAKE_INSTALL_PREFIX:FILEPATH=${CMAKE_CURRENT_BINARY_DIR}/eigen/install
+        -DCMAKE_INSTALL_PREFIX:FILEPATH=${EIGEN3_INSTALL_DIR}
         -DBUILD_TESTING:BOOL=OFF
+        -DEIGEN_BUILD_TESTING:BOOL=OFF
         -DEIGEN_BUILD_PKGCONFIG:BOOL=OFF
+        -DEIGEN_BUILD_DOC:BOOL=OFF
         -DEIGEN_TEST_NOQT:BOOL=ON
 )
 
-set(Eigen3_DIR ${CMAKE_CURRENT_BINARY_DIR}/eigen/install)
+set(Eigen3_DIR ${EIGEN3_INSTALL_DIR})
+set(EIGEN_CMAKE ${EIGEN3_INSTALL_DIR}/share/eigen3/cmake)
